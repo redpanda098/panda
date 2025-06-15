3360610a41502d0c170a89409f1e4ca01d1a9bb2
redpanda098 <tanishkajadhav819@gmail.com>
2025-06-15 15:58:10 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,8 @@
+var name = "tanishka"
+var age = 16
+var Skills = "HTML and CSS"
+
+
+console.log("tanishka: " , name);
+console.log("16: " , age);
+console.log("html and css: " , Skills);
---
+++ second.js
@@ -0,0 +1,11 @@
+var a=10, b=3
+
+console.log(a+b); // 13
+console.log(a-b); // 7
+console.log(a*b); // 30
+console.log(a/b); // 3.33...
+console.log(a%b); // 1
+ 
+var x=5, y="5"
+console.log(x == y); // true
+console.log(x != y); // false
