a351783db74491445a9a8da0ff988f8966bf8e83
redpanda098 <tanishkajadhav819@gmail.com>
2025-06-21 15:54:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// defining part
+function greet(name){
+    console.log("hello" , name)
+
+}
+greet("world");
+// calling part
+
+function cube(a){
+    return a*a*a
+}
+ans1 = cube(2)
+console.log(ans1)
+
+function sum(a,b){
+    return a+b
+}
+ans2 = sum(2,3)
+console.log(ans2)
