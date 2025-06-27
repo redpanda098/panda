90949f27b5304eeac02a2e730749ad7005a19818
redpanda098 <tanishkajadhav819@gmail.com>
2025-06-27 20:14:44 +0530

    Add files via upload

---
+++ p2.js
@@ -0,0 +1,16 @@
+var a="2" , b="4", c="6";
+function add(x, y) {
+  return x + y;
+}
+function multiply(x, y) {
+  return x * y;
+}
+function subtract(x, y) {
+    return x - y;
+    }
+function divide(x, y) {
+  if (y === 0) {
+    throw new Error("Cannot divide by zero");
+  }
+  return x / y;
+}
