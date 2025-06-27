20df7fd774ac9aa8ea2f8b746e1c42d7b2d4f7d7
redpanda098 <tanishkajadhav819@gmail.com>
2025-06-27 20:06:56 +0530

    Add files via upload

---
+++ p1.js
@@ -0,0 +1,28 @@
+const day = "tuesday";
+
+switch (day) {
+    case "monday":
+        console.log("It's Monday, start of the week!");
+        break;
+    case "tuesday":
+        console.log("It's Tuesday, keep going!");
+        break;
+    case "wednesday":
+        console.log("It's Wednesday, halfway through!");
+        break;
+    case "thursday":
+        console.log("It's Thursday, almost there!");
+        break;
+    case "friday":
+        console.log("It's Friday, the weekend is near!");
+        break;
+    case "saturday":
+        console.log("It's Saturday, enjoy your weekend!");
+        break;
+    case "sunday":
+        console.log("It's Sunday, rest and recharge!");
+        break;
+    default:
+        console.log("Invalid day, please enter a valid day of the week.");  
+        break;
+}   
