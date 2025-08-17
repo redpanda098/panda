8e348a6109239b2abda1b9e49278f4c521181fd3
redpanda098 <tanishkajadhav819@gmail.com>
2025-08-17 15:50:25 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,35 @@
+print("tanishka")
+
+name="tanishka"
+age=17
+hobbies="reading and coding"
+ 
+print(f"My name is {name}, I am {age} years old, and my hobbies include {hobbies}.")
+
+# activity.......###
+# printing a greeting message
+print("hello, world!")
+
+# printing my name
+print("My name is Tanishka")
+
+#printing a fun fact about myself
+print("i love writing novels,stories and poems")
+
+# performing and printing a simple calculation
+print("The sum of 5 and 3 is:", 5 + 3)
+
+## activity......###
+#print a sentence
+print("I am learning Python programming.")
+
+#print a number
+print(5)
+
+#next line after print
+print("hello python \n")
+
+#printing all the keywords present in python
+print("keywords in python are...")
+import keyword
+print(keyword.kwlist)
